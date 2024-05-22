d7178b478b2cd114f5156633156ce3e421531826
khaiwhan <sritonchai.p@gmail.com>
2024-05-22 19:03:03 +0700

    update profile home

---
+++ script.js
@@ -0,0 +1,14 @@
+const about_me = {
+    name: "Khaiwhan 🐣",
+    skills: [
+        "Angular", "Express", "Python", "scss",
+        "sass", ".NET", "MySQL", "SQL Server",
+        "Postgres", "Redis", "Docker", "Linux"
+    ],
+    jobPosition: "Full-Stack Developer",
+    hardWorker: true,
+    quickLearner: true,
+    problemSolver: true,
+    teamManagement: true,
+    hireable: () => this.hardWorker && this.teamManagement
+}

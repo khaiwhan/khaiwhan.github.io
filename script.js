12a52471cd8d0cb741877a274e451d35441bdeaf
khaiwhan <sritonchai.p@gmail.com>
2024-05-28 03:31:25 +0700

    update profile

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const load = () => {
     generateHome()
+    sec2contentMouseEvent()
 }
 
 const generateHome = () => {
@@ -18,6 +19,32 @@ const generateHome = () => {
         }
         i++;
     }, 50);
+}
+
+const sec2contentMouseEvent = () => {
+    // const elements = document.querySelectorAll('.sec2-content');
+
+    // elements.forEach(element => {
+    //     element.addEventListener('mouseover', (e) => {
+    //         const { offsetX, offsetY } = calPositionSec2ContentMouseEvent(e.offsetX, e.offsetY)
+    //         element.style.setProperty("--sec2-content-box-shadow", `${offsetX}px ${offsetY}px`);
+    //     });
+
+    //     element.addEventListener('mouseout', () => {
+    //         element.style.setProperty("--sec2-content-box-shadow", `0px 0px`);
+    //     });
+    // })
+}
+
+const calPositionSec2ContentMouseEvent = (x, y) => {
+    let { offsetX, offsetY } = ""
+
+    if (x.toString().charAt(0) == "-") offsetX = x.toString().substring(1, 2)
+    else offsetX = `-${x.toString().charAt(0)}`
+    if (y.toString().charAt(0) == "-") offsetY = y.toString().substring(1, 2)
+    else offsetY = `-${y.toString().charAt(0)}`
+
+    return { offsetX, offsetY }
 }
 
 load()
